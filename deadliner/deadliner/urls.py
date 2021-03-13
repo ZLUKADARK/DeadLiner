0546b66ce6fdc30841cd8a73ccf3e994aa6608b1
ZLUKADARK <48823161+ZLUKADARK@users.noreply.github.com>
2021-03-13 19:00:37 +0600

    Login Stap

--- deadliner/deadliner/urls.py
+++ deadliner/deadliner/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    
+    
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    
+    path('api/v1/auth/user/', include('user.urls')),
+
  ]
